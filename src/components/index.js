1037c3196fe759e0f426d7e022fbbc44e3b2d8bd
capregsoft <ualtaf238@gmail.com>
2022-09-29 20:40:16 +0500

    Basic Sturucture done

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Feed from "./Feed";
+import ChannelDetail from "./ChannelDetail";
+import SearchFeed from "./SearchFeed";
+import VideoDetail from "./VideoDetail";
+import Navbar from "./Navbar";
+
+export { Feed,ChannelDetail,SearchFeed,VideoDetail,Navbar};
